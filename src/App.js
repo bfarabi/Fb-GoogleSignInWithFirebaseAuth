1ab33ce9b1384292aac75cce887d078bf6089867
Billal Farabi <bfarabi108@gmail.com>
2021-11-28 12:19:58 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 function App() {
+  const [user, setUser] = useState({})
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+  
+  const handleGoogleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email);
+      });
+  };
+  const handleFbSignIn =()=>{
+    firebase
+  .auth()
+  .signInWithPopup(fbProvider)
+  .then((result) => {
+    var credential = result.credential;
+    var user = result.user;
+    var accessToken = credential.accessToken;
+    console.log(user);
+    setUser(user);
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    console.log(errorCode, errorMessage, email, credential);
+  });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn} >Sign in using google</button>
+      <br />
+      <button onClick={handleFbSignIn} >Sign in using Facebook</button>
+      <h1>Name: {user.displayName}</h1>
+      <img src={user.photoURL} alt="" />
+
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDK9SvBKV4cIBd9zSP6IVL4EwzDNodhzbs",
+  authDomain: "fir-auth-recap-6c0be.firebaseapp.com",
+  projectId: "fir-auth-recap-6c0be",
+  storageBucket: "fir-auth-recap-6c0be.appspot.com",
+  messagingSenderId: "448501066852",
+  appId: "1:448501066852:web:0d8f03e38488815a324371"
+};
+  export default firebaseConfig;
